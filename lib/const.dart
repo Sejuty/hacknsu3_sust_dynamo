a637f4efd77e93ff33a1bdd20900f57b0f15b538
Sourav9063 <53114581+Sourav9063@users.noreply.github.com>
2021-11-26 10:11:17 +0600

    init main

---
+++ lib/const.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+late double screenH;
+late double screenW;
+const textColor = Color(0xff7F858C);
+const textColor2 = Color(0xff7F858C);
+const kprimaryColor = Color(0xff0077B6);
+
+var textStyle = GoogleFonts.roboto(
+    fontSize: 12.0,
+    letterSpacing: 1.5,
+    fontWeight: FontWeight.bold,
+    color: textColor.withOpacity(1.0));
